2113115c698d59cdf206bbd37b1aedee9a7dd93f
Eduardo Rodrigues de Lima <146451946+NightEdu@users.noreply.github.com>
2024-11-23 15:24:19 -0300

    commit-12 (23-11-2024)
    poke-card cor

--- frontend/src/components/PokeCard.jsx
+++ frontend/src/components/PokeCard.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import fetchData from "../utils/fetchData.js";
-
-import { EntryPropType } from "../types.js";
-import PropTypes from 'prop-types'
-import '../styles/pokecard.css'
+import PropTypes from "prop-types";
+import "../styles/pokecard.css";
 
 function PokeCard({ innerRef, entry }) {
   const [pokemon, setPokemon] = useState(null);
@@ -18,37 +16,42 @@ function PokeCard({ innerRef, entry }) {
     getPokeData();
   }, [entry]);
 
-  if (pokemon?.is_default != true) return null;
+  if (pokemon?.is_default !== true) return null;
+
+  const mainType = pokemon?.types[0]?.type?.name || "normal"; // Obtém o tipo principal
 
   return (
-    <div className="poke-card" ref={innerRef ? innerRef : undefined}>
-      <NavLink to={`/entry/${pokemon.name}`}>
-        <img
-          className="poke-card-image"
-          src={pokemon?.sprites.other.home.front_default}
-          alt={pokemon?.name + " image."}
-        />
-      </NavLink>
-      <span className="poke-card-span">{`N° ${pokemon?.id
-        .toString()
-        .padStart(4, "0")}`}</span>
-      <h2 className="poke-card-name">{pokemon.species.name}</h2>
-      <ul className="poke-card-types">
-        {pokemon?.types.map((type) => (
-          <li
-            className={`poke-card-type type-badge-${type.type.name}`}
-            key={type.slot}
-          >
-            {type.type.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <div
+  className={`poke-card type-${pokemon?.types[0]?.type.name}`}
+  ref={innerRef ? innerRef : undefined}
+>
+  <NavLink to={`/entry/${pokemon.name}`}>
+    <img
+      className="poke-card-image"
+      src={pokemon?.sprites.other.home.front_default}
+      alt={pokemon?.name + " image."}
+    />
+  </NavLink>
+  <span className="poke-card-span">{`N° ${pokemon?.id
+    .toString()
+    .padStart(4, "0")}`}</span>
+  <h2 className="poke-card-name">{pokemon.species.name}</h2>
+  <ul className="poke-card-types">
+    {pokemon?.types.map((type) => (
+      <li
+        className={`poke-card-type type-badge-${type.type.name}`}
+        key={type.slot}
+      >
+        {type.type.name}
+      </li>
+    ))}
+  </ul>
+</div>
   );
 }
 
 PokeCard.propTypes = {
-  entry: EntryPropType.isRequired,
+  entry: PropTypes.object.isRequired,
   innerRef: PropTypes.func,
 };
 
