1a966dc8e1c2972228ce5cbb3140bf1286571ff8
Arthur Rocha <arrosante@gmail.com>
2024-11-26 10:42:16 -0300

    commit-18 (26-11-2024)

--- frontend/src/pages/Landpage.jsx
+++ frontend/src/pages/Landpage.jsx
@@ -1,36 +1,47 @@
-import { NavLink } from 'react-router-dom'
-import '../styles/landpage.css'
+import { NavLink } from "react-router-dom";
+import "../styles/landpage.css";
 
 function Landpage() {
-    return (
-        <div id='landpage'>
-            <div className="landpage-container">
-                <div className="navbar">
-                    <img src="/pokemon-logo.png" alt='pokémon logo' draggable='false' />
-                    <h2> TeamBuilder | Feito por <a href="https://github.com/Arthurr-Rosante" target='a_blank'>Arthur</a> e <a href="https://github.com/NightEdu" target='a_blank'>Eduardo</a>
-                    </h2>
-                    <NavLink to='/authenticate/login'>
-                        <span>
-                            Entrar <i className='bx bx-log-in'></i>
-                        </span>
-                    </NavLink>
-                </div>
-                <div className='hero'>
-                    <span>PIKACHU</span>
-                    <span>PIKACHU</span>
-                    <span>PIKACHU</span>
-                    <div>
-                        <h1>Monte seu Time Conosco!</h1>
-                        <div className='hero-buttons'>
-                            <NavLink to="/authenticate/register">Cadastrar-se</NavLink>
-                            <NavLink to="/authenticate/login">Logar-se</NavLink>
-                        </div>
-                    </div>
-                    <img className='hero-img' src="/pikachu-hero.webp" alt="pikachu hero" draggable='false' />
-                </div>
-            </div>
+  return (
+    <div id="landpage">
+      <div className="landpage-container">
+        <div className="navbar">
+          <img src="/pokemon-logo.png" alt="pokémon logo" draggable="false" />
+          <h2>
+            {" "}
+            TeamBuilder | Feito por{" "}
+            <a href="https://github.com/Arthurr-Rosante" target="a_blank">
+              Arthur
+            </a>{" "}
+            e{" "}
+            <a href="https://github.com/NightEdu" target="a_blank">
+              Eduardo
+            </a>
+          </h2>
         </div>
-    )
+        <div className="hero">
+          <span>PIKACHU</span>
+          <span>PIKACHU</span>
+          <span>PIKACHU</span>
+          <div>
+            <h1>Monte seu Time Conosco!</h1>
+            <div className="hero-buttons">
+              <NavLink to="/authenticate/register">Cadastrar-se</NavLink>
+              <NavLink to="/authenticate/login">Logar-se</NavLink>
+            </div>
+          </div>
+          <div className="hero-img-container">
+            <img
+              className="hero-img"
+              src="/pikachu-hero.webp"
+              alt="pikachu hero"
+              draggable="false"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Landpage
+export default Landpage;
